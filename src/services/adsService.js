9c4584b74f48c9c1baf570bbcae33e9bec41caca
namnt0413 <namnt0413@gmail.com>
2023-02-12 22:07:48 +0700

    update

--- src/services/adsService.js
+++ src/services/adsService.js
@@ -2,7 +2,7 @@
 
 import db, { sequelize } from '../models/index';
 require('dotenv').config();
-import _, { result } from 'lodash';
+import _, { result, toArray } from 'lodash';
 
 let getAllAds = () => {
     return new Promise( async (resolve, reject) => {
@@ -10,7 +10,10 @@ let getAllAds = () => {
             let data = await db.Advertisement.findAll({
                 attributes: {
                     exclude: ['createdAt','updatedAt']
-                }
+                },
+                order: [
+                    ['id', 'DESC'],
+                ]
             });
             resolve(data)
 
@@ -33,12 +36,16 @@ let getCurrentAds = () => {
                     finishedAt: { 
                         [Op.gte]: new Date() 
                     }
-                  }
+                } ,
+                include: [
+                    { model: db.ads_product, as: 'adsData', attributes: ['product_id'] },
+                ],
+                raw: false,
+                nest: true,
              });
-           
-            console.log(data)
+            
             resolve(data)
-
+            
         } catch (error) {
             reject(error);
         }
@@ -59,6 +66,7 @@ let createAds = (data) => {
                     name: data.name,
                     content: data.content,
                     type: data.type,
+                    status: 0,
                     visitTime: 0,
                     image: data.image,
                     startedAt: data.startedAt,
@@ -81,7 +89,6 @@ let createAds = (data) => {
 }
 
 let createAdsProduct = (data) => {
-    // console.log(data);
     return new Promise( async (resolve, reject) => {
         try {
             if( data.type == 4) {
@@ -162,8 +169,8 @@ let updateAds = (data) => {
                 where: { id : data.id},
                 raw: false // tat hien thi object cua sequelize di
             })
-            if( data.image !== '' ) {
-                advertisement.image = data.image;
+            if( data.image === '' ) {
+                data.image = advertisement.image ;
             }
 
             if( advertisement.type == 4 ){
@@ -245,168 +252,6 @@ let editAds = (adsId) => {
     })
 }
 
-
-
-let createAdsApi = (data) => {
-    return new Promise( async (resolve, reject) => {
-        try {
-            if( !data.name || !data.content || !data.startedAt || !data.finishedAt ) {
-                resolve({
-                    error_code: 1,
-                    error_msg: 'Missing required parameter'
-                });
-            } else {
-                await db.Advertisement.create({
-                    name: data.name,
-                    content: data.content,
-                    visitTime: 0,
-                    startedAt: data.startedAt,
-                    finishedAt: data.finishedAt
-                })
-
-                resolve({
-                    error_code: 0,
-                    error_msg: 'Create new advertisement successfully'
-                })
-            }
-
-        } catch (error) {
-            reject(error);
-        }
-    })
-}
-
-let getAllAdsApi = () => {
-    return new Promise( async (resolve, reject) => {
-        try {
-            let data = await db.Advertisement.findAll({
-                attributes: {
-                    exclude: ['createdAt','updatedAt']
-                }
-            });
-            // if(data && data.length > 0) {
-            //     data.map( item => {
-            //         item.image = Buffer.from(item.image,'base64').toString('binary'); // convert image to base64
-            //         return item;
-            //     })
-            // }
-            // console.log(data);
-            resolve({
-                error_code: 0,
-                error_msg: 'OK',
-                data
-            })
-
-        } catch (error) {
-            reject(error);
-        }
-    })
-}
-
-let deleteAdsApi = (adsId) => {
-    return new Promise( async (resolve, reject) => {
-        try {
-            let advertisement = await db.Advertisement.findOne({
-                where: { id : adsId }
-            })
-            if(!advertisement) {
-                resolve({
-                    error_code:2,
-                    error_msg:`The advertisement isn't exist`
-                })
-            }
-            // await user.destroy(); khong sd duoc vi da config lai sequelize ra dang object raw o nodejs
-            // con ham duoi chay duoc vi xoa truc tiep o db
-            await db.Advertisement.destroy({
-                where: { id : adsId }
-            })
-
-            resolve({
-                error_code:0,
-                error_msg:`The advertisement was deleted`
-            })
-            
-        } catch (error) {
-            reject(error);
-        }
-    })
-}
-
-let updateAdsApi = (data) => {
-    return new Promise( async (resolve, reject) => {
-        try {
-            if(!data.id){
-                resolve({
-                    error_code:2,
-                    error_msg: 'Missing required parameter'
-                })
-            }
-            
-            let advertisement = await db.Advertisement.findOne({ // where id = data.id
-                where: { id : data.id},
-                raw: false // tat hien thi object cua sequelize di
-            })
-
-           if( advertisement){
-            advertisement.name = data.name;
-            advertisement.content = data.content;
-            advertisement.visitTime = data.visitTime;
-            advertisement.startedAt = data.startedAt;
-            advertisement.finishedAt = data.finishedAt;
-
-            await advertisement.save(); // luu vao database , doc docs
-                resolve({
-                    error_code: 0,
-                    error_msg: 'Update advertisement successfully'
-                })
-           }
-           else{
-                resolve({
-                    error_code: 1,
-                    error_msg: 'Update advertisement failed'
-                });
-           } 
-        } catch (e) {
-            reject(e);
-        }
-    })
-}
-
-let getDetailAdsApi = (adsId) => {
-    return new Promise( async (resolve, reject) => {
-        try {
-            if( !adsId ) {
-                resolve({
-                    error_code: 1,
-                    error_msg: 'Missing required parameter',
-                })
-            } else {
-
-                let data = await db.Advertisement.findOne({ 
-                    where: { 
-                        id : adsId 
-                    },
-                    attributes: {
-                        exclude: ['createdAt','updatedAt']
-                    }
-                })
-                // data.image = Buffer.from(data.image,'base64').toString('binary'); // convert image to base64
-            
-                resolve({
-                    error_code: 0,
-                    error_msg: 'OK',
-                    data: data,
-                })
-            }
-
-        } catch (error) {
-            reject(error);
-        }
-    })
-}
-
-
 module.exports = {
     getAllAds, getCurrentAds , createAds, createAdsProduct,  deleteAds, updateAds, editAds,
-    createAdsApi, getAllAdsApi, deleteAdsApi, updateAdsApi, getDetailAdsApi
 }
