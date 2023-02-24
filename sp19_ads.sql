bd01fd467e2f37729ea5a7a229707bb0d0916eb3
namnt0413 <namnt0413@gmail.com>
2023-02-24 23:04:56 +0700

    update db

--- sp19_ads.sql
+++ sp19_ads.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 09, 2023 at 12:23 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Generation Time: Feb 24, 2023 at 05:03 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,11 +40,36 @@ CREATE TABLE `ads_products` (
 --
 
 INSERT INTO `ads_products` (`id`, `ads_id`, `product_id`, `createdAt`, `updatedAt`) VALUES
-(35, 75, 4, '2023-01-09 17:11:17', '2023-01-09 11:11:25'),
-(36, 76, 3, '2023-01-09 10:57:56', '2023-01-09 10:57:56'),
-(44, 77, 1, '2023-01-09 11:18:57', '2023-01-09 11:18:57'),
-(45, 77, 3, '2023-01-09 11:18:57', '2023-01-09 11:18:57'),
-(46, 77, 5, '2023-01-09 11:18:57', '2023-01-09 11:18:57');
+(35, 75, 2, '2023-01-09 17:11:17', '2023-01-10 16:54:46'),
+(36, 76, 3, '2023-01-09 10:57:56', '2023-01-10 16:54:58'),
+(54, 97, 3, '2023-01-09 17:49:35', '2023-01-11 18:33:23'),
+(61, 98, 4, '2023-01-10 17:31:50', '2023-01-11 18:30:15'),
+(65, 99, 1, '2023-01-10 17:39:20', '2023-01-10 17:39:20'),
+(66, 99, 3, '2023-01-10 17:39:20', '2023-01-10 17:39:20'),
+(67, 100, 3, '2023-01-11 18:58:41', '2023-01-11 18:58:41'),
+(68, 100, 5, '2023-01-11 18:58:41', '2023-01-11 18:58:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ads_vouchers`
+--
+
+CREATE TABLE `ads_vouchers` (
+  `id` int(11) NOT NULL,
+  `ads_id` int(11) NOT NULL,
+  `voucher_id` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ads_vouchers`
+--
+
+INSERT INTO `ads_vouchers` (`id`, `ads_id`, `voucher_id`, `createdAt`, `updatedAt`) VALUES
+(1, 1, 2, '2023-02-24 17:03:11', '2023-02-24 17:03:11'),
+(2, 1, 4, '2023-02-24 17:03:11', '2023-02-24 17:03:11');
 
 -- --------------------------------------------------------
 
@@ -57,23 +82,26 @@ CREATE TABLE `advertisements` (
   `name` varchar(255) DEFAULT NULL,
   `content` varchar(255) DEFAULT NULL,
   `visitTime` int(11) DEFAULT 0,
-  `image` longblob DEFAULT NULL,
+  `image` text DEFAULT NULL,
   `startedAt` datetime DEFAULT NULL,
   `finishedAt` datetime DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   `type` varchar(255) DEFAULT NULL,
-  `product_id` varchar(255) DEFAULT NULL
+  `status` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `advertisements`
 --
 
-INSERT INTO `advertisements` (`id`, `name`, `content`, `visitTime`, `image`, `startedAt`, `finishedAt`, `createdAt`, `updatedAt`, `type`, `product_id`) VALUES
-(75, 'Test ads 2', 'Chuong trinh khuyen mai ngay 12/12', 0, NULL, '2022-12-12 07:30:00', '2023-01-08 18:30:00', '2023-01-09 17:11:14', '2023-01-09 11:11:25', '1', NULL),
-(76, 'Test ads 4', 'test ads 7 content', 0, NULL, '2023-01-03 17:57:00', '2023-01-08 17:57:00', '2023-01-09 10:57:52', '2023-01-09 10:57:52', '2', NULL),
-(77, 'Test ads 7', 'test ads 7 content', 0, NULL, '2023-01-01 00:00:00', '2023-01-14 19:00:00', '2023-01-09 11:02:01', '2023-01-09 11:18:57', '4', NULL);
+INSERT INTO `advertisements` (`id`, `name`, `content`, `visitTime`, `image`, `startedAt`, `finishedAt`, `createdAt`, `updatedAt`, `type`, `status`) VALUES
+(75, 'Test ads 2', 'Chuong trinh khuyen mai ngay 12/12', 0, '\\uploads\\5152550ca958939a269d6f10b186e300', '2022-12-12 07:30:00', '2023-01-08 18:30:00', '2023-01-09 17:11:14', '2023-01-10 16:54:46', '1', 1),
+(76, 'Test ads 4', 'test ads 7 content', 0, '\\uploads\\68beac543df1c2af2e3def658d5cbe1a', '2023-01-03 17:57:00', '2023-01-08 17:57:00', '2023-01-09 10:57:52', '2023-01-10 16:54:58', '2', 1),
+(97, 'Test edit ads image', 'aaaaaaaa', 0, '\\uploads\\5cafab6d8dfa5152e5fd64ff22731b29', '2023-01-10 00:49:00', '2023-02-14 00:56:00', '2023-01-09 17:49:34', '2023-01-11 18:33:23', '1', 0),
+(98, 'Test product 4', 'Chuong trinh khuyen mai ngay 12/12', 0, '\\uploads\\e74e5190a74bcbcb65a6f4bf27bd00cf', '2023-01-11 00:31:00', '2023-01-21 00:31:00', '2023-01-10 17:31:45', '2023-01-11 18:30:15', '3', 0),
+(99, 'aaaa', 'aaaaaaaaa', 0, '\\uploads\\e6d5d5f44eec44140ba1941af7080941', '2022-11-01 00:39:00', '2022-12-09 00:39:00', '2023-01-10 17:39:16', '2023-01-10 17:39:16', '4', 1),
+(100, 'Khuyen mai ban quan ao Shoppe', 'Chuong trinh khuyen mai ngay 6/1', 0, '\\uploads\\4ec63810c71cedcc29458ddaf0538094', '2023-01-06 18:00:00', '2023-01-13 07:00:00', '2023-01-11 18:58:37', '2023-01-11 18:58:37', '4', 0);
 
 -- --------------------------------------------------------
 
@@ -105,6 +133,12 @@ ALTER TABLE `ads_products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `ads_vouchers`
+--
+ALTER TABLE `ads_vouchers`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `advertisements`
 --
 ALTER TABLE `advertisements`
@@ -125,13 +159,19 @@ ALTER TABLE `sequelizemeta`
 -- AUTO_INCREMENT for table `ads_products`
 --
 ALTER TABLE `ads_products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+
+--
+-- AUTO_INCREMENT for table `ads_vouchers`
+--
+ALTER TABLE `ads_vouchers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `advertisements`
 --
 ALTER TABLE `advertisements`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
